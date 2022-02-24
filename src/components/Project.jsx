67d0114df7c4bd77724b4ccf03935f8106855105
False Start <caroline.leopold@gmail.com>
2022-02-24 12:46:17 -0500

    mapped first level data

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,17 @@
 import projects from "../data";
 import "../utils.css"
 
-const projectCard = projects.map(project =>
+const projectCard = projects.map(project => 
   <li key={project.id}>
     <div className="flex-c">
       <div key={project.company}>{project.company}</div>
-      <div key={project.category}>{project.category}</div>
     </div>
+
   </li>
 );
+
+ {/* <div key={project.category}>{project.category}</div>
+    </div> */}
 
 export function Project () {
   return (
