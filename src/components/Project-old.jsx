66a94217db677621075cb1e0b4d0d3197fb04f3e
False Start <caroline.leopold@gmail.com>
2022-02-24 13:13:35 -0500

    nested map test

---
+++ src/components/Project-old.jsx
@@ -0,0 +1,28 @@
+import projects from "../data";
+import "../utils.css"
+
+const projectCard = projects.map(project => 
+  <li key={project.id}>
+    <div className="flex-c">
+      <div key={project.company}>{project.company}</div>
+    </div>
+
+  </li>
+);
+
+ {/* <div key={project.category}>{project.category}</div>
+    </div> */}
+
+export function Project () {
+  return (
+    <div>
+          <ul className="card">
+            <div className="auto-grid">
+              {projectCard}
+            </div>
+          </ul>
+    </div>
+  );
+  
+
+}
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,33 +1,62 @@
-import projects from "../data";
+// import projects from "../data";
 import "../utils.css"
 
-const projectCard = projects.map(project => 
-  <li key={project.id}>
-    <div className="flex-c">
-      <div key={project.company}>{project.company}</div>
-    </div>
-
-  </li>
-);
-
- {/* <div key={project.category}>{project.category}</div>
-    </div> */}
-
 export function Project () {
+  const projects = [
+    {
+      company: "Benefiber",
+      product: "branding",
+      category: "copywriting",
+      tests: [
+        { name: "aaa", type: "aaa-type"},
+        {name: "bbb", type: "bbb-type"}
+      ],
+      tags: [
+        "large brand",
+        "B2C",
+        "health"
+      ]
+    },
+    {
+      company: "Ingram Micro",
+      product: "thought leadership",
+      category: "enterprise marketing",
+      tests: [
+        {name: "myName", type: "myType"},
+        {name: "Alias", type: "aliatype"}
+      ],
+      tags: [
+        "digital magazine",
+        "Microsoft Azure"
+      ]
+    },
+    {
+      company: "Reward Expert",
+      product: "founder interviews",
+      category: "SEO content",
+      tests: [
+        {name: "testy", type: "A"},
+        {name: "meme", type: "B"}
+      ],
+      tags: "travel-tech"
+    }
+  ];
+
   return (
     <div>
-          <ul className="card">
-            <div className="auto-grid">
-              {projectCard}
-            </div>
-          </ul>
-    </div>
+      {projects.map((project, index) => (
+        <div key={index}>
+          <h2>Company: {project.company}</h2>
+          <div>
+            {project.tests.map((test, i) => (
+              <p key={i}>
+                {test.name} typed {test.type}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
+      </div>
   );
-  
 
-}
-
-
-
-
-
+            }
