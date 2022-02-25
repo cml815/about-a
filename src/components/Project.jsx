e7356ede24e5df97197ed7556809dc0bdbd088e6
False Start <caroline.leopold@gmail.com>
2022-02-25 10:25:27 -0500

    fix vc autocorrect error

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,7 @@ export function Project () {
       company: "Benefiber",
       product: "branding",
       category: "copywriting",
-      DataTransferItemList: [
+      tests: [
         { name: "aaa", type: "aaa-type"},
         {name: "bbb", type: "bbb-type"}
       ],
