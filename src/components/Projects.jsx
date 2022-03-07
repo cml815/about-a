7583f3ecdb5bd3d055b219717f09a86c25579818
False Start <caroline.leopold@gmail.com>
2022-03-07 18:35:04 -0500

    add return to map fn

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,22 +6,24 @@ export function Projects () {
   let projects = getProjects();
    return (
     <div className="autoGrid">
-      {projects.map((projects, index) => (
-        <div className="card" key={index}>
-          <h2>Company: {projects.company}</h2>
-          <ul className="flex-c tag">
-            <li className="tags">Tags: {projects.tags}</li>
-          </ul>
-          <div>
-            {projects.tests.map((test, i) => (
-              <p key={i}>
-                {test.name} typed {test.type}
-              </p>
-            ))}
-          </div>
+      {projects.map((projects, index) => {
+        return (
+          <div className="card" key={index}>
+            <h2>Company: {projects.company}</h2>
+            <ul className="flex-c tag">
+              <li className="tags">Tags: {projects.tags}</li>
+            </ul>
+            <div>
+              {projects.tests.map((test, i) => (
+                <p key={i}>
+                  {test.name} typed {test.type}
+                </p>
+              ))}
+            </div>
 
-        </div>
-      ))}
+          </div>
+        );
+      })}
       </div>
   );
             }
