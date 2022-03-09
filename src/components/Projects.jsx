a6cbf256ec81c1fd547838b726591560a49d5d3e
False Start <caroline.leopold@gmail.com>
2022-03-09 11:33:30 -0500

    add card border

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,18 +8,20 @@ export function Projects () {
     <div className="autoGrid">
       {projects.map((projects, index) => {
         return (
-          <div className="card" key={index}>
-            <p className="label"><span>{projects.product}</span></p> 
-            <h2>{projects.company}</h2> 
-            {/* <ul className="flex-c tag">
-              <li className="tags">Tags: {projects.tags}</li>
-            </ul> */}
-            <div>
-              {projects.tests.map((test, i) => (
-                <p key={i}>
-                  {test.name} typed {test.type}
-                </p>
-              ))}
+          <div className="cardContainer">
+            <div className="card" key={index}>
+              <p className="label"><span>{projects.product}</span></p> 
+              <h2>{projects.company}</h2> 
+              {/* <ul className="flex-c tag">
+                <li className="tags">Tags: {projects.tags}</li>
+              </ul> */}
+              <div>
+                {projects.tests.map((test, i) => (
+                  <p key={i}>
+                    {test.name} typed {test.type}
+                  </p>
+                ))}
+              </div>
             </div>
 
           </div>
