0ed45aa9918812c425b131c87ac1cbf47a64cac6
False Start <caroline.leopold@gmail.com>
2022-03-16 15:09:35 -0400

    build project view

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { getProject } from '../data';
+import "../utils.css";
+import "../App.css";
 
 export function Project() {
   let params = useParams();
   let project = getProject(params.projectId);
   return (
     <>
-    <h2>Project: {project.company}</h2>
+    <main className="projectContainer">
+      <h2>Project: {project.company}</h2>
+    </main>
     
+
     </>
   )
   
