738b7a89b4c17a50d096ee3261790dc53f85af7e
False Start <caroline.leopold@gmail.com>
2022-03-17 22:40:49 -0400

    start TopNav CSS module

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from "react-router-dom";
 // import { About } from "./routes/about";
+import styles from './TopNav.module.css';
 import "../utils.css";
 
 export default function TopNav() {
   return (
-    <nav className="">
+    <nav className={styles.TopNav}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
