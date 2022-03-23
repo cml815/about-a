7cbffd74de88e6526403937c2d3a8b9cfe614049
False Start <caroline.leopold@gmail.com>
2022-03-23 16:11:22 -0400

    refactor project display

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,40 +7,29 @@ import Sidebar from './Sidebar';
 import { getProject } from '../data';
 import ProjectImage from './ProjectImage';
 
-
-// function ProfilePage() {
-//   // Get the userId param from the URL.
-//   let { userId } = useParams();
-//   // ...
-// }
-
-// import { useParams } from "react-router-dom";
-
-// export default function Invoice() {
-//   let params = useParams();
-//   return <h2>Invoice: {params.invoiceId}</h2>;
-// }
-
 export function Project() {
   let params = useParams();
   // let project = getProject();
   let project = getProject(params.projectId);
   return (
     <>
-    <div className="projectContainer">
-      <div class="flex">
-        <Sidebar />
+    <main className={styles.wrapper}>
+      <Sidebar />
+      <section className={styles.projectContainer}>
+        <div className={styles.header}>
+          
+          <h2>Project ID: {params.projectId}</h2>
+          <p>Product: {project.product}</p>
+      </div>
+        <div className={styles.container}>
+          <p>Category: {project.category}</p>
         <img src={project.image} width="500" />
-      </div>
-      <div className={styles.container}>
-        <h2>Project ID: {params.projectId}</h2>
-        <p>Product: {project.product}</p>
-      </div>
 
-    <main>
-        
+        </div>
+          
+        </section>  
+      
     </main>
-    </div>
     </>
   )
   
