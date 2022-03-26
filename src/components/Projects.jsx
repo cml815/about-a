64fcdb7a626e7d23d4e19662d82f5894dcf1766a
False Start <caroline.leopold@gmail.com>
2022-03-26 14:04:48 -0400

    added header container for home page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,35 +7,43 @@ import '../global.css';
 export function Projects () {
   let projects = getProjects();
    return (
-    <div className="autoGrid">
-      {projects.map((projects, index) => {
-        return (
-          <div className="cardContainer">
-            <div className="card" key={index}>
-              <p className="label"><span>{projects.product}</span></p>
-              <img className="cardImage" src={projects.image} alt="project image" /> 
-              {/* <ul className="flex-c tag">
-                <li className="tags">Tags: {projects.tags}</li>
-              </ul> */}
-              <div>
-                {projects.tests.map((test, i) => (
-                  <p key={i}>
-                    {test.name} typed {test.type}
-                  </p>
-                ))}
-              </div>
-              <Link className="cardLinks"
-               to={`/projects/${projects.company}`}
-               key={projects.company}
-               >
-                 <h4>{projects.company}</h4>
-              </Link>
-            </div>
-          </div>
-        );
-      })}
-      <Outlet />
-      </div>
-  );
-            }
+     <>
+      <section className="header">
+        <h1>This is the projects header</h1>
+        <p>Add some information here</p>
+      </section>
+      <div className="autoGrid">
+        {projects.map((projects, index) => {
+          return (
+            <>    
+            <ul className="cardContainer" role="list">
+              <figure className="card" key={index}>
+                <p className="label"><span>{projects.product}</span></p>
+                <img className="cardImage" src={projects.image} alt="project image" /> 
+                {/* <ul className="flex-c tag">
+                  <li className="tags">Tags: {projects.tags}</li>
+                </ul> */}
+                <div>
+                  {projects.tests.map((test, i) => (
+                    <p key={i}>
+                      {test.name} typed {test.type}
+                    </p>
+                  ))}
+                </div>
+                <Link className="cardLinks"
+                to={`/projects/${projects.company}`}
+                key={projects.company}
+                >
+                  <h4>{projects.company}</h4>
+                </Link>
+              </figure>
+            </ul>
+            </>
+          );
+        })}
+        <Outlet />
+        </div>
+    </>
+);
+          }
 
