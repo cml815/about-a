b138766ea4943f59f94aa5123dd9f28f51ae01ef
False Start <caroline.leopold@gmail.com>
2022-06-22 12:27:13 -0400

    hero styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,89 +1,89 @@
 import { Link, NavLink, Outlet, useSearchParams } from 'react-router-dom';
-import {getProjects} from "../data";
-import "../utils.css";
-import "../App.css";
+import {getProjects} from '../data';
+import '../utils.css';
+import '../App.css';
 import '../global.css';
 
 export function Projects () {
   let projects = getProjects();
   let [searchParams, setSearchParams] = useSearchParams();
    return (
-     <>
-     <div classname="hero">
-      <h1>Writer's Portfolio</h1>
-      <h2>Subheading</h2>
-      <p>Hi, I'm Caroline Leopold, a writer who ties health, technology &amp; interested parties. </p>
-     </div>
-      <nav className="header">
-        <h1>This is the projects nav header</h1>
-        <input
-          value={searchParams.get("filter") || ""}
-          onChange={(event) => {
-            let filter = event.target.value;
-            if (filter) {
-              setSearchParams({ filter });
-            } else {
-              setSearchParams({});
-            }
-          }}
-        />
-         {projects
-          .filter((project) => {
-            let filter = searchParams.get("filter");
-            if (!filter) return true;
-            let name = project.name.toLowerCase();
-            return name.startsWith(filter.toLowerCase());
-          })
-          .map((project) => (
-            <NavLink
-              style={({ isActive }) => ({
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : "",
-              })}
-              to={`/projects/${project.category}`}
-              key={project.id}
-            >
-              {project.category}
-            </NavLink>
-          ))}
-      </nav>
-      <div className="autoGrid">
-        {projects.map((projects, index) => {
-          return (
-            <>    
-            <ul className="cardContainer" role="list">
-              <li>
-              <figure className="card" key={index}>
-                <div className="label">
-                  <p>{projects.product}</p>
-                </div>
-                <img className="cardImage" src={projects.image} alt="project image" /> 
-                {/* <ul className="flex-c tag">
-                  <li className="tags">Tags: {projects.tags}</li>
-                </ul> */}
-                <div>
-                  {projects.tests.map((test, i) => (
-                    <p key={i}>
-                      {test.name} typed {test.type}
-                    </p>
-                  ))}
-                </div>
-                <Link className="cardLinks"
-                to={`/projects/${projects.company}`}
-                key={projects.company}
-                >
-                  <h4>{projects.company}</h4>
-                </Link>
-              </figure>
-              </li>
-            </ul>
-            </>
-          );
-        })}
-        <Outlet />
+    <div className="projects-wrapper">
+      <section className="hero container">
+        <h1>Writer's Portfolio</h1>
+        <h2>Subheading</h2>
+        <p>Hi, I'm Caroline Leopold, a writer who ties health, technology &amp; interested parties. </p>
+      </section>
+        <nav className="header">
+          <h1>This is the projects nav header</h1>
+          <input
+            value={searchParams.get("filter") || ""}
+            onChange={(event) => {
+              let filter = event.target.value;
+              if (filter) {
+                setSearchParams({ filter });
+              } else {
+                setSearchParams({});
+              }
+            }}
+          />
+          {projects
+            .filter((project) => {
+              let filter = searchParams.get("filter");
+              if (!filter) return true;
+              let name = project.name.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            })
+            .map((project) => (
+              <NavLink
+                style={({ isActive }) => ({
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                })}
+                to={`/projects/${project.category}`}
+                key={project.id}
+              >
+                {project.category}
+              </NavLink>
+            ))}
+        </nav>
+        <div className="autoGrid">
+          {projects.map((projects, index) => {
+            return (
+              <>    
+              <ul className="cardContainer" role="list">
+                <li>
+                <figure className="card" key={index}>
+                  <div className="label">
+                    <p>{projects.product}</p>
+                  </div>
+                  <img className="cardImage" src={projects.image} alt="project image" /> 
+                  {/* <ul className="flex-c tag">
+                    <li className="tags">Tags: {projects.tags}</li>
+                  </ul> */}
+                  <div>
+                    {projects.tests.map((test, i) => (
+                      <p key={i}>
+                        {test.name} typed {test.type}
+                      </p>
+                    ))}
+                  </div>
+                  <Link className="cardLinks"
+                  to={`/projects/${projects.company}`}
+                  key={projects.company}
+                  >
+                    <h4>{projects.company}</h4>
+                  </Link>
+                </figure>
+                </li>
+              </ul>
+              </>
+            );
+          })}
+          <Outlet />
         </div>
-    </>
+    </div>
 );
           }
 
