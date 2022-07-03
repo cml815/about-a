4f830f27df17b31000705b04b60330dc674107ea
False Start <caroline.leopold@gmail.com>
2022-07-02 23:31:07 -0400

    add new data fields

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,8 +29,13 @@ export function Project() {
         <div className={styles.container}>
           <p>Category: {project.category}</p>
           <img src={project.image} width="800" />
-          <p>{project.snippet}</p>
-          <a href={project.url}>Visit website</a>
+          <p className={styles.textCol}>{project.snippet}</p>
+          <h3>Read on {project.company}'s website</h3>
+          <div className={styles.urlList}>
+            <a href={project.url0}>{project.title0}</a>
+            <a href={project.url1}>{project.title1}</a>
+            <a href={project.url2}>{project.title2}</a>          
+          </div>
           
         </div>
         </main>
--- src/data.js
+++ src/data.js
@@ -17,7 +17,10 @@ let projects = [
     ],
     image: require("./images/benefiber-aging.png"),
     snippet: "Benefiber. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
-    url: "https://www.benefiber.com/fiber-in-your-life/fiber-and-wellness/aging-and-digestion/"
+    title0: "Aging and Digestion",
+    title1: "What Does the Gut-Brain Connection Mean",
+    url0: "https://www.benefiber.com/fiber-in-your-life/fiber-and-wellness/aging-and-digestion/",
+    url1: "https://www.benefiber.com/digestive-health/gut-health-101/what-is-gut-brain-connection/",
   },
   {
     id: "2",
@@ -35,7 +38,12 @@ let projects = [
     ],
     image: require("./images/health.png"),
     snippet: "Colgate Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
-    url: ""
+    url0: "https://www.colgate.com/en-us/oral-health/bad-breath/how-to-help-bad-breath-go-away-under-stress",
+    title0: "How to Get Rid of Stress-Induced Bad Breath",
+    url1: "https://www.colgate.com/en-us/oral-health/tooth-sensitivity/what-causes-sensitive-teeth",
+    title1: "What Causes Tooth Sensitivity & How to Treat It",
+    url2: "https://www.colgate.com/en-us/oral-health/adult-orthodontics/tips-for-adults-with-braces",
+    title2: "Tips For Adults With Braces"
   },
   {
     id: "3",
