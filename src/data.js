56e0dd313c4a82e0ed6f7ca339056d22b98900b5
False Start <caroline.leopold@gmail.com>
2022-07-04 12:26:48 -0400

    completed snippets

--- src/data.js
+++ src/data.js
@@ -5,10 +5,10 @@ let projects = [
     company: "Benefiber",
     theme: "#00ad42",
     product: "major brand",
-    category: "CategoryXXX",
+    category: "campaign",
     tags: [
       {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
+      {tag: "supplement", type: ""}
     ],
     // tags: [
     //   "tagXXX",
@@ -16,7 +16,7 @@ let projects = [
     //   "health"
     // ],
     image: require("./images/benefiber-aging.png"),
-    snippet: "Benefiber. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Benefiber launched a campaign to position the fiber supplement as top-of-mind for active people. I wrote a series of articles on Benefiber's website",
     title0: "Aging and Digestion",
     title1: "What Does the Gut-Brain Connection Mean",
     url0: "https://www.benefiber.com/fiber-in-your-life/fiber-and-wellness/aging-and-digestion/",
@@ -26,20 +26,15 @@ let projects = [
   },
   {
     id: "2",
-    company: "Colgate",
+    company: "Colgate®",
     product: "major brand",
-    category: "copywriting",
+    category: "campaign",
     tags: [
       {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
-    ],
-    tags: [
-      "large brand",
-      "B2C",
-      "health"
+      {tag: "relatable", type: ""}
     ],
     image: require("./images/colgate.png"),
-    snippet: "Colgate Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Colgate® used a content strategy to make the oral health line more relatable and trustworthy. What the do especially well is offering authoritative information and product information in one place.",
     url0: "https://www.colgate.com/en-us/oral-health/bad-breath/how-to-help-bad-breath-go-away-under-stress",
     title0: "How to Get Rid of Stress-Induced Bad Breath",
     url1: "https://www.colgate.com/en-us/oral-health/tooth-sensitivity/what-causes-sensitive-teeth",
@@ -51,10 +46,10 @@ let projects = [
     id: "3",
     company: "Medical News Today",
     product: "patient information",
-    category: "authority",
+    category: "authority site",
     tags: [
       {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
+      {tag: "E-A-T", type: ""}
     ],
     image: require("./images/mnt.png"),
     snippet: "Medical News Today Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
@@ -68,14 +63,16 @@ let projects = [
     id: "4",
     company: "Reward Expert",
     product: "founder interviews",
-    category: "SEO content",
+    category: "SEO",
     tags: [
-      {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
+      {tag: "link building", type: ""},
+      {tag: "travel", type: ""},
+      {tag: "startups", type: ""},
+      {tag: "interviews", type: ""}
     ],
     // tags: "travel-tech",
     image: require("./images/reward-expert.png"),
-    snippet: "Reward Expert Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Reward Expert's active content creation and SEO has placed the website among the top in its category. As a way of building relationships, the site published interviews of CEOs of growing travel tech startups.<br> I interviewed and wrote stories on 30+ startups, including ones featured in Mashable, TechCrunch,and Fast Company.",
     url0: "https://www.rewardexpert.com/blog/homeexchange-how-travelers-can-get-free-lodging/",
     title0: "Home Exchange: How Travelers Can Get Free Lodging",
     url1: "https://www.rewardexpert.com/blog/vacatia-family-friendly-resort-rentals/",
@@ -93,18 +90,18 @@ let projects = [
   {
     id: "5",
     company: "Overture / Johnson & Johnson",
-    product: "medical writing",
-    category: "too many cats",
+    product: "biotech investment news",
+    category: "magazine",
     tags: [
-      {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
+      {tag: "biotech", type: ""},
+      {tag: "innovation", type: ""}
     ],
     // tags: [
     //   "",
     //   ""
     // ],
     image: require("./images/overture.png"),
-    snippet: "Medical News Today Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Johnson & Johnson sponsored this article on technology to detect cancer more quickly.",
     url0: "https://www.overtureglobal.io/story/liquid-biopsies-for-cancer-detection-move-closer-to",
     title0: "Liquid Biopsies for Cancer Detection Move Closer to Reality",
     url1: "",
@@ -112,43 +109,25 @@ let projects = [
   },
   {
     id: "6",
-    company: "DriversEd",
-    product: "conversion",
-    category: "authority",
+    company: "Aceable",
+    product: "pillar content",
+    category: "SEO",
     tags: [
-      {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
+      {tag: "ecommerce", type: ""},
+      {tag: "automotive", type: ""}
     ],
     // tags: [
     //   "",
     //   ""
     // ],
     image: require("./images/driversed.png"),
-    snippet: "Medical News Today Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Aceable and its sub-brands (Drivers Ed & I Drive Safely) provide online driving courses need a strong web presence to be found. As a writer, I have been supported their long-running campaigns to be relevant and easily found in a highly competitive industry.",
     url0: "https://driversed.com/trending/ten-driving-mistakes-you-dont-know-youre-making/",
     title0: "Ten Driving Mistakes You Don’t Know You’re Making",
-    url1: "https://www.medicalnewstoday.com/articles/312292",
-    title1: "What Does a Bone Marrow Biopsy Involve?"
-  },
-  {
-    id: "7",
-    company: "I Drive Safely",
-    product: "patient information",
-    category: "authority",
-    tags: [
-      {tag: "health", type: ""},
-      {tag: "web copy", type: ""}
-    ],
-    // tags: [
-    //   "",
-    //   ""
-    // ],
-    image: require("./images/idrivesafely.png"),
-    snippet: "Medical News Today Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
-    url0: "https://www.idrivesafely.com/defensive-driving/trending/tips-and-tricks-become-safer-and-more-confident-driver",
-    title0: "Tips and Tricks to Become a Safer and More Confident Driver",
-    url1: "https://www.medicalnewstoday.com/articles/312292",
-    title1: "What Does a Bone Marrow Biopsy Involve?"
+    url1: "https://www.aceable.com/blog/what-keep-your-cars-emergency-kit-winter/",
+    title1: "What to Keep in Your Car's Emergency Kit",
+    url2: "https://www.idrivesafely.com/defensive-driving/trending/tips-and-tricks-become-safer-and-more-confident-driver",
+    title2: "Tips and Tricks to Become a Safer and More Confident Driver",
 
   },
   {
@@ -160,10 +139,6 @@ let projects = [
       {tag: "health", type: ""},
       {tag: "web copy", type: ""}
     ],
-    // tags: [
-    //   "",
-    //   ""
-    // ],
     image: require("./images/mnt.png"),
     snippet: "Medical News Today Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
     url0: "https://www.medicalnewstoday.com/articles/313265",
@@ -174,14 +149,14 @@ let projects = [
   {
     id: "9",
     company: "Net Health",
-    product: "conversion copy",
-    category: "authority",
+    product: "eBooks",
+    category: "lead generation",
     tags: [
       {tag: "EMR"},
       {tag: "covid-19"},
     ],
     image: require("./images/net-health.png"),
-    snippet: "Net Health Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Net Health offers custom electronic health/medical records for some of the fastest growing specialities, including Urgent Care. Their approach to attracting sales is by offering clear, concise ebooks and other collateral.",
     url0: "https://www.nethealth.com/how-employee-health-can-ease-provider-burnout/",
     title0: "How Employee Health Can Ease Provider Burnout",
     url1: "https://www.nethealth.com/6-key-performance-indicators-for-your-occupational-medicine-practice/",
@@ -192,33 +167,16 @@ let projects = [
     title3: "3 Ways to Keep Track of EH During Covid-19/Flu Season"
   },
   {
-    id: "10",
-    company: "Aceable",
-    product: "patient information",
-    category: "authority",
-    tags: [
-      {tag: "health"},
-      {tag: "web copy"}
-    ],
-    image: require("./images/aceable.png"),
-    snippet: "Aceable Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
-    url0: "https://www.aceable.com/blog/what-keep-your-cars-emergency-kit-winter/",
-    title0: "What to Keep in Your Car's Emergency Kit",
-    url1: "https://www.medicalnewstoday.com/articles/312292",
-    title1: "What Does a Bone Marrow Biopsy Involve?"
-
-  },
-  {
     id: "11",
     company: "T2",
     product: "case study",
-    category: "authority",
+    category: "lead generation",
     tags: [
       {tag: "AWS migration"},
-      {tag: "web copy"}
+      {tag: "tech"}
     ],
     image: require("./images/t2.png"),
-    snippet: "Aceable Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Many like read testimonials before they buy. For tech companies, buyers are often high-level specialists who prefer case studies to see how complex adoptions work in real life.",
     url0: "https://t2tech.us/client-stories/",
     title0: "AWS Migration",
   },
@@ -233,7 +191,7 @@ let projects = [
       {tag: "vet practice"}
     ],
     image: require("./images/talkatoo.png"),
-    snippet: "Aceable Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo vel fringilla est ullamcorper eget nulla. In fermentum posuere urna nec tincidunt praesent semper.",
+    snippet: "Talkatoo has great word-of-month from vets who love the dictation software. As a writer, I was asked to convert a video interview into a compelling case study.",
     url0: "https://talkatoo.com/news/category/case-studies/",
     title0: "Dictation Software Helps Vet Practice",
   },
