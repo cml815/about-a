3be51a9c0a7503509ecfda627ad5c0daa4e099d1
False Start <caroline.leopold@gmail.com>
2022-08-25 13:16:11 -0400

    rm nav js

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,9 +28,6 @@ export default function Sidebar() {
           </NavLink>
         ))}
       </nav>
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-      </a>
       <Outlet />
     </div>
   );
