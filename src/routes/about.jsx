0f350300ab8d8b732320c5fee3a2faba231d5e03
False Start <caroline.leopold@gmail.com>
2022-10-30 19:46:03 -0400

    add About copy

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,14 +1,19 @@
 
 
+import "../App.css";
+import "../global.css";
+
 export default function About () {
   return (
     <main>
-      <h1>Caroline Leopold</h1>
-      <h2>Medical and Marketing Writer</h2>
-      <p>My interest in medicine and training in research and statistics led to a science writing career.</p>
-      <h2>Most desirable skills</h2>
+      <h1>Caroline Leopold: Medical and Marketing Writer</h1>
+      <h2>Current Work</h2>
+      <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
+      <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
+      <h2>Industries and specialties.</h2>
       <p>Track record with grant writing.</p>
       <p>This website shares my writing and web design work from the past 10 years.</p>
     </main>
   )
+
 }
