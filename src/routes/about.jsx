9faef8e95e67e47102234620a4a9f3a2996879df
False Start <caroline.leopold@gmail.com>
2022-10-30 20:08:58 -0400

    add copy

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -8,13 +8,16 @@ export default function About () {
     <main className="centered">
       <div className="inner">
         <h1>Caroline Leopold: Medical and Marketing Writer</h1>
-        <h2>Current Work</h2>
+        <h2>Writes Visions for Better Lives</h2>
         <p>As a consultant, I engage numerous clients on urgent fund development or marketing projects.</p> 
         <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
         <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
-        <h2>Industries and specialties.</h2>
-        <p>Track record with grant writing.</p>
+        <h2>Data Present Well Persuades</h2>
+        <p>What unites my grant and marketing writing is that I make persuasive arguments grounded in data, research and practical concerns.</p>
+        <h2>Recent Successes</h2>
         <p>This website shares my writing and web design work from the past 10 years.</p>
+        <p>web design work from the past 10 years.</p>
+        <p>web design work from the past 10 years.</p>
       </div>
     </main>
   )
