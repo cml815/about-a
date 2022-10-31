0f2996713f5b444d5ce42e4aa0c4edd40cca9e33
False Start <caroline.leopold@gmail.com>
2022-10-31 12:55:34 -0400

    about styles

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,14 +1,20 @@
-
-
 import "../App.css";
 import "../global.css";
 
 export default function About () {
   return (
     <main className="centered">
-      <div className="inner">
+      <div className="flex">
         <h1>Caroline Leopold: Medical and Marketing Writer</h1>
-        <p>As a consultant, I engage numerous clients on urgent fund development or marketing projects.</p> 
+       
+        <div className="container">
+          <img src="pro-pic-twitter.jpg" width="400"/>
+        </div>
+      </div>
+      <div> 
+      <div className="container">
+          <p>As a consultant, I engage numerous clients on urgent fund development or marketing projects. At<a href="https://www.fuelcopy.com"> Fuel Copy</a>, I map out strategy and create custom communications to meet revenue goals.</p>
+        </div>
         <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
         <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
         <h2>Collaborative creations</h2>
