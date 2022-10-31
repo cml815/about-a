6ba0c255f3e33492d2042441357874d7468f431d
False Start <caroline.leopold@gmail.com>
2022-10-30 20:49:06 -0400

    add copy

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -8,16 +8,18 @@ export default function About () {
     <main className="centered">
       <div className="inner">
         <h1>Caroline Leopold: Medical and Marketing Writer</h1>
-        <h2>Writes Visions for Better Lives</h2>
         <p>As a consultant, I engage numerous clients on urgent fund development or marketing projects.</p> 
         <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
         <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
-        <h2>Data Present Well Persuades</h2>
+        <h2>Collaborative creations</h2>
         <p>What unites my grant and marketing writing is that I make persuasive arguments grounded in data, research and practical concerns.</p>
-        <h2>Recent Successes</h2>
-        <p>This website shares my writing and web design work from the past 10 years.</p>
-        <p>web design work from the past 10 years.</p>
-        <p>web design work from the past 10 years.</p>
+        <p>My projects often engage various stakeholders on important subjects. Stemming a pandemic. Relieving the shortage of nurses. Finding treatments for serious illnesses. </p>
+        <h2>Work highlights</h2>
+        <ul>
+        <li><p>$14.5M in grants for innovative COVID-19 projects</p></li>
+        <li><p>Helped 16 small businesses win 13M in SBIR funding. </p></li>
+        <li><p>Content writing published on global brand websites and influential websites.</p></li>
+        </ul>
       </div>
     </main>
   )
