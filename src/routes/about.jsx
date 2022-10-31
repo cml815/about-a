a624467bdf1351beba1f0e120a5273233a039d3b
False Start <caroline.leopold@gmail.com>
2022-10-31 19:12:56 -0400

    shift elements

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,23 +1,27 @@
 import "../App.css";
 import "../global.css";
+import profile from "./pro-pic-twitter.jpg"
 
 export default function About () {
   return (
     <main className="centered">
       <div className="flex">
-        <h1>Caroline Leopold: Medical and Marketing Writer</h1>
-       
-        <div className="container">
-          <img src="pro-pic-twitter.jpg" width="400"/>
-        </div>
+        <h1>Caroline Leopold <br />Medical and Marketing Writer</h1>
       </div>
-      <div> 
-      <div className="container">
+      <div className="inner container">
           <p>As a consultant, I engage numerous clients on urgent fund development or marketing projects. At<a href="https://www.fuelcopy.com"> Fuel Copy</a>, I map out strategy and create custom communications to meet revenue goals.</p>
         </div>
-        <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
-        <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
-        <h2>Collaborative creations</h2>
+        <div className="flex">
+          <div className="split-container">
+            <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
+            <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
+          </div>
+          <div className="container">
+            <img src={profile} alt="Caroline Leopold" width="300"/>
+          </div>
+        </div>
+        <div>
+        <h2>Collaborative creator</h2>
         <p>What unites my grant and marketing writing is that I make persuasive arguments grounded in data, research and practical concerns.</p>
         <p>My projects often engage various stakeholders on important subjects. Stemming a pandemic. Relieving the shortage of nurses. Finding treatments for serious illnesses. </p>
         <h2>Work highlights</h2>
