f08a3bb3775a751a09184901967f8f1eb50a7e2c
False Start <caroline.leopold@gmail.com>
2022-11-01 19:40:29 -0400

    rs

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,5 +1,6 @@
 import "../App.css";
 import "../global.css";
+import "../utils.css";
 import profile from "./pro-pic-twitter.jpg"
 
 export default function About () {
@@ -13,23 +14,22 @@ export default function About () {
         </div>
         <div className="flex-m">
           <div className="split-container">
-          <p>As a consultant, I engage numerous clients on urgent grant and marketing projects. At<a href="https://www.fuelcopy.com"> Fuel Copy</a>, I map out strategy and create custom communications to meet revenue goals.</p>
+            <p>As a consultant, I engage numerous clients on urgent grant and marketing projects. At<a href="https://www.fuelcopy.com"> Fuel Copy</a>, I map out strategy and create custom communications to meet revenue goals.</p>
             <p>About 75% of my time is writing grant proposals for universities, national non-profits and medical centers. The subject matter ranges from health system innovations, medical education, public health, and biomedical research.</p>
             <p>My remaining time is spent writing digital marketing content largely in health, technology and automotive and travel.</p>
           </div>
           <div className="container">
             <img src={profile} alt="Caroline Leopold" width="200"/>
           </div>
-        
-          <div class="flex-m">
+          </div>
+          <div className="">
           <h4>Work highlights</h4>
-          <ul>
-            <li><p>$14.5M in grants for innovative COVID-19 projects</p></li>
-            <li><p>Helped 16 small businesses win 13M in SBIR funding. </p></li>
-            <li><p>Content writing published on global brand websites and influential websites.</p></li>
+          <ul className="flex">
+            <li className="third card-bordered"><p className="short-caption">$14.5M in grants for innovative COVID-19 projects</p></li>
+            <li className="third card-bordered"><p className="short-caption">Helped 16 small businesses win 13M in SBIR funding. </p></li>
+            <li className="third card-bordered"><p className="short-caption">Content writing published on global brand websites and influential websites.</p></li>
         </ul>
           </div>
-        </div>
     </main>
   )
 
