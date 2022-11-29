369d4b0d0773182d6bdc5e22a8e75f943e595d24
False Start <caroline.leopold@gmail.com>
2022-11-29 12:32:39 -0500

    rm commented out code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,6 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="contact flex-m">
-        {/* <div className="">
-          <p>Ask me (almost) anything!</p>
-          <p><span>Email&nbsp;<a href="mailto:caroline@fuelcopy.com">caroline@fuelcopy.com</a></span></p>
-        </div>
-        <div className="socials flex-c">
-          <a href="https://www.linkedin.com/in/cleopold"><p>LinkedIn</p></a>
-          <a href="https://www.github.com/cml55"><p>Github</p></a>
-        </div> */}
-
-      </div>
       
       <div>
         <p className="center">&copy;{(new Date().getFullYear())} Caroline Leopold</p>
