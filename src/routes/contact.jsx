9aec6d144a85627d942ccbdf9af2ac63fd1b334d
False Start <caroline.leopold@gmail.com>
2022-11-28 21:30:44 -0500

    add copy

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -4,9 +4,11 @@ export default function Contact () {
       <div className="">
         <h2>Let's chat</h2>
         <p>I'm open to talk about the craft and economy of writing. <a href="mailto:caroline@fuelcopy.com">Email</a>&nbsp;is the best way to reach me. </p>
+        <p>It's great if you have questions about medical writing or grant writing. My expertise is with projects in the United States. My understanding of the landscape outside my country's borders is limited. </p>
         <p>
-          You may be curious about this website. I have been creating websites as a hobby and feel free to browse this site's Github repo. It's a dead simple build with React and a JSON data file for the portfolio entries. 
+          You may be curious about this website. I have been making sites as a hobby and feel free to browse this site's Github repo. It's a dead simple build with React and a JSON data file for the portfolio entries.
         </p>
+        <p>Fingers crossed, I plan to build a proper portfolio database using PostgresSQL.</p>
       </div>
     </main>
   )
